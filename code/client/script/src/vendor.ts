6d6b02ad815fea6fd18a84d4dcd1090060c613d0
jsuzuki20120311 <shukatu.2012.5.25+github@gmail.com>
2017-02-04 16:49:34 +0900

    ライブラリをビルドするためのエントリポイントとなるTypeScriptファイルを追加。

---
+++ code/client/script/src/vendor.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+// RxJS
+import 'rxjs';
+
+import 'jquery';
