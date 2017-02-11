34f1804835ef8396cf6d24a6082fd2c3ce813fdd
jsuzuki20120311 <shukatu.2012.5.25+github@gmail.com>
2017-02-11 20:34:08 +0900

    不要なimport文を削除。

--- code/client/script/src/app.module.ts
+++ code/client/script/src/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './component/app.component';
 import { ArticleListComponent } from './component/ArticleList.component';
 import { ArticleEditorComponent } from './component/ArticleEditor.component';
 import { UpdateArticlePageComponent } from './component/update-article-page.component';
-import { Config } from './Config';
 
 @NgModule({
   bootstrap: [ AppComponent ],
