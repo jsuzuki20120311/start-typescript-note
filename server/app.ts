0e1fad6bb9b533761073508205e32ab42fe90088
jsuzuki20120311 <shukatu.2012.5.25+github@gmail.com>
2017-02-20 02:09:31 +0900

    アプリケーションの設定とサーバーの起動処理を別ファイルに分割。

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,6 @@
 import * as bodyParser from 'body-parser';
 import * as ejs from 'ejs';
 import * as express from 'express';
-import * as http from 'http';
 import * as path from 'path';
 import * as favicon from 'serve-favicon';
 import api from './routes/api';
@@ -50,69 +49,4 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
   }
 });
 
-
-const server = http.createServer(app);
-const port = normalizePort(process.env.PORT || '3000');
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-
-/**
- * Normalize a port into a number, string, or false.
- */
-function normalizePort(val: string) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-
-/**
- * Event listener for HTTP server "error" event.
- */
-function onError(error: any) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr.port;
-  console.log(addr + bind);
-}
+export default app;
